5e30ddaf115dec5da5ba92046c615a94b0fd2452
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-30 01:58:01 +0300

    final commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
+import NavBar from "./NavBar";
 
 function Home() {
   const [users, setUsers] = useState([])
@@ -9,12 +9,12 @@ function Home() {
     fetch("http://localhost:4000/users")
       .then(r => r.json())
       .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>
-  });
+      .catch(error => console.error(error))
+  }, [])
+
+//   const userList = users.map(user =>{
+//     return <UserCard key={user.id} user={user}/>
+//   });
 
   return (
     <>
@@ -24,7 +24,10 @@ function Home() {
       </header>
       <main>
         <h1>Home!</h1>
-        {userList}
+        {/* {userList} */}
+        {users.map((user)=>{
+            return <UserCard key={user.id} user={user.id} />
+        })}
       </main>
     </>
   );
